355cc031c445f6673c90a7d24e9f67cf9bea7a94
kov-edit <kovacs.edit@diak.szbi-pg.hu>
2025-10-15 12:25:18 +0200

    1225

---
+++ vue_projekt/firstsfc/src/App.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Köszönet az oldalon'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message}}</h1>
+
+  <h1>Gyümölcsök</h1>
+  <div id="wrapper">
+    <food-item 
+      food-name="Barack"
+      :is-favorite="true"/>
+    <food-item 
+      food-name="Körte"
+      food-desc="Ez egy citrom, de hosszabb a szöveg"/>
+    <food-item 
+      food-name="Narancs"
+      food-desc="Ez egy citrom és lorem ipsum dolor sit amet consectetur adipisicing elit."
+      :is-favorite="false"/>
+  </div>
+</template>
+
+<style>
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#wrapper > div{
+  border: dashed 3px purple;
+  flex-basis: 140px;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgray;
+}
+
+#wrapper > div:hover{
+  cursor: pointer;
+  background-color: rgb(250, 193, 160);
+  border-color: rgb(73, 1, 1);
+}
+</style>
---
+++ vue_projekt/firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+    /*data() {
+        return {
+            name: 'Barack',
+            message: 'Ez egy citrom',
+            counter: 0
+        }
+    },
+    methods: {
+        adjHozza() {
+            this.counter+=15
+        }
+    }*/
+    /*props: [
+        'foodName',
+        'foodDesc',
+        'isFavorite'
+    ]*/
+    props: {
+        foodName: {
+            type: String,
+            required: true
+        },
+        foodDesc: {
+            type: String,
+            required: true,
+            default: 'Minden egy citrom',
+            validator: function(value) {
+                if (value.length > 20 && value.length < 50) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    data() {
+        return {
+            foodIsFavorite: this.isFavorite
+        }
+    },
+    methods: {
+        toggleFavorite() {
+            this.foodIsFavorite = !this.foodIsFavorite
+        }
+   }
+}
+</script>
+
+<template>
+    <div @click="adjHozza()">
+        <h2>
+            {{ foodName }}
+            <img src="../img_quality.svg" v-show="foodIsFavorite">
+        </h2>
+        <p>{{ foodDesc }}</p>
+        <button @click="toggleFavorite">Magic gomb</button>
+        <!--<p>{{ message }}</p>
+        <p id="szamolo">Kattintások: {{ counter }}</p>-->
+    </div>
+</template>
+
+<style>
+img {
+    height: 1.5em;
+    float: right;
+}
+button {
+    background-color: darkorchid;
+    color: azure;
+    border: solid 2px rgb(61, 45, 45);
+    padding: 5px 10px;
+}
+button:hover {
+    cursor: pointer;
+    background-color: azure;
+    color: darkorchid;
+}
+/*#szamolo {
+    font-weight: bold;
+    color: saddlebrown;
+}*/
+</style>
